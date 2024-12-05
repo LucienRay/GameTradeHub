99e2e60d95c60aafb2b175c3d3d7da631158694d
Andy Chen <andychen@Andys-MacBook-Air.local>
2024-12-05 16:49:17 +0800

    Add toolbar component, build.bash

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -4,11 +4,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios  from "axios";
-
+import ToolBar from "./components/ToolBar.vue";
 axios.defaults.withCredentials = true;
 
 const app = createApp(App)
 
+app.component("ToolBar", ToolBar);
 app.use(router)
 
 app.mount('#app')
