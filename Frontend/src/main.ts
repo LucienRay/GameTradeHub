588e3cb6e4bb4c4bb60d79b520c80190af3c0d6a
111590045 <t111590045@ntut.org.tw>
2024-12-12 13:36:30 +0800

    Fix bad import of ChatWidget.

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -3,15 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios  from "axios";
+import axios from "axios";
 import ToolBar from "./components/ToolBar.vue";
 import SearchBar from './components/SearchBar.vue';
+import ChatWidget from './components/ChatWidget.vue';
 axios.defaults.withCredentials = true;
 
 const app = createApp(App)
 
 app.component("ToolBar", ToolBar);
 app.component("SearchBar", SearchBar)
+app.component("ChatWidget", ChatWidget)
 app.use(router)
 
 app.mount('#app')
