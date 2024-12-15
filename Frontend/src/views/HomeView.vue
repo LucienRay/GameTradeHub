cf8c96e0647f32a67d49942db9eabf63fecb01e3
LucienRay <ray93022@gmail.com>
2024-12-15 18:29:25 +0800

    add logout button in toolBar

--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -18,41 +18,16 @@ import axios from 'axios';
 import { useRouter } from "vue-router";
 
 const router = useRouter();
-const Nickname = ref('');
-const isAuthenticated = ref(false);
+
 let games = ref([{ ID: 0, Name: '', Platform: '', Image: '' }]);
 
 onMounted(() => {
-  // 驗證用戶登入狀態
-  axios.post('/api/auth')
-    .then(response => {
-      axios.post('/api/get/userINFO').then(response => {
-        Nickname.value = response.data.Nickname;
-        isAuthenticated.value = true;
-      }).catch(error => {
-        isAuthenticated.value = false;
-      });
-    })
-    .catch(() => {
-      isAuthenticated.value = false; // 如果驗證失敗
-    });
-
   axios.post('/api/get/GameINFO').then(response => {
     games.value = response.data;
   }).catch(error => {
     console.log(error);
   });
 });
-
-function logout() {
-  axios.post('/api/logout')
-    .then(() => {
-      isAuthenticated.value = false;
-    })
-    .catch(() => {
-      console.log('Logout failed');
-    });
-}
 
 function routeToGame(gameName: string) {
     // alert(`Routing to ${gameName} `);
